73ac0bdb2e755c354f96dca44874980ccde7905b
Andini Permata Dewanti <10231014@student.itk.ac.id>
2026-03-18 23:51:26 +0700

    update main.py

--- backend/main.py
+++ backend/main.py
@@ -158,9 +158,9 @@ def team_info():
     return {
         "team": "cloud-team-XX",
         "members": [
-            {"name": "Nama 1", "nim": "NIM1", "role": "Lead Backend"},
-            {"name": "Nama 2", "nim": "NIM2", "role": "Lead Frontend"},
-            {"name": "Nama 3", "nim": "NIM3", "role": "Lead DevOps"},
-            {"name": "Nama 4", "nim": "NIM4", "role": "Lead QA & Docs"},
+            {"name": "Andini Permata Dewanti", "nim": "10231014", "role": "Lead Backend"},
+            {"name": "Putri Rahmawati", "nim": "10231074", "role": "Lead Frontend"},
+            {"name": "Desnita Dwi Putri", "nim": "10231030", "role": "Lead DevOps"},
+            {"name": "Krishandy Dhanysa Pratama", "nim": "10231050", "role": "Lead QA & Docs"},
         ],
     }
