efc70662a7076fe57281780c560cf005907878d8
Krishandy Dhanysa Pratama <10231050@student.itk.ac.id>
2026-04-15 16:48:18 +0800

    Update docker-run.sh

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -35,7 +35,7 @@ case $ACTION in
     docker run -d \
       --name backend \
       --network cloudnet \
-      --env-file backend/.env.docker \
+      --env-file .env.docker \
       -p 8000:8000 \
       cloudapp-backend:v1
     
